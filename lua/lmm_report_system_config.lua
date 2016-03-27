c84375559a54f268c91a20a425be6bfb1a74bcc3
Matt S <XxLMM13xXgaming@gmail.com>
2016-03-27 14:57:47 -0400

    Added some options

--- lua/lmm_report_system_config.lua
+++ lua/lmm_report_system_config.lua
@@ -4,9 +4,9 @@ LMMRSConfig.DevMode = false
 */
 LMMRSConfig.AdminGroups = {"owner", "superadmin", "admin"}
 
-LMMRSConfig.PlayerReportReasons = {"RDM", "FailRP", "Mic Spam", "Prop Spam"} -- Do not add a other option!
+LMMRSConfig.PlayerReportReasons = {"RDM", "FailRP", "Mic Spam", "Prop Spam", "Minge", "Troll"} -- Do not add a other option!
 
-LMMRSConfig.TimeForCooldown = .5 -- Amount in min for the cooldown
+LMMRSConfig.TimeForCooldown = 5 -- Amount in min for the cooldown
 --Notify systems:
 --darkrp (darkrp notify)
 --text (text notify)
